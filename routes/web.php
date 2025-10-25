8358b8992b458750be2a1922c19956e976317faa
Rashedul_Islam <rashedul.book@gmail.com>
2025-10-26 00:06:50 +0600

    view

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
- Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
  
