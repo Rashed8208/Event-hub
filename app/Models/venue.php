62e3b048b450f5fa072ecc77fd34186e0e037b54
hsn771 <hr492785@gmail.com>
2025-10-30 13:11:23 +0600

    ven

--- app/Models/venue.php
+++ app/Models/venue.php
@@ -9,4 +9,9 @@ class venue extends Model
 {
     use HasFactory;
     protected $fillable=['name','description','address','city','capacity','price_per_day','image','status'];
+    
+     public function getImageAttribute($value)
+    {
+        return $value ? url('/' . $value) : null;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserAuthController;
 use App\Http\Controllers\Api\EventController;
-
+use App\Http\Controllers\Api\VenueController;
+use App\Http\Controllers\Api\TicketBookingController;
+use App\Http\Controllers\Api\WishlistController;
 // Public routes
 Route::post('register',[UserAuthController::class,'register']);
 Route::post('login',[UserAuthController::class,'login']);
@@ -15,4 +17,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('events', EventController::class);
+    Route::apiResource('venue', VenueController::class);
+    Route::apiResource('ticket-booking', TicketBookingController::class);
+    Route::apiResource('wishlist', WishlistController::class);
 });
